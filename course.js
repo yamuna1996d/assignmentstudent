8f37ece046d20f9a03841f1572e16474692db5da
yamuna1996d <yamunag19@gmail.com>
2020-04-27 22:40:38 +0530

    first commit

--- course.js
+++ course.js
@@ -7,7 +7,7 @@ var router = require('./routes/rou');
 app.use(parser.urlencoded({extended:false}));
 app.use('/add',router);
 
-mongoose.connect("mongodb+srv://userdb:ava@1996@cluster0-lbzwy.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://userdb:ava1996@cluster0-lbzwy.mongodb.net/test?retryWrites=true&w=majority");
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server Started");
 });
